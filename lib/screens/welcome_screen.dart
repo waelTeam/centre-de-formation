c6bad59ed15c8bc9ab205911c61ece47f3769530
eya gueddes <eyagueddes1@gmail.com>
2021-07-27 12:14:17 +0100

    commit 1

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
 class WelcomeScreen extends StatefulWidget {
-  const WelcomeScreen({Key? key}) : super(key: key);
+  static const String id ='welcome';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
